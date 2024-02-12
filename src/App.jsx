4ae37ef5c99d19f06a19c54f818efe85404d9b95
kapilkumar7 <kapil.kumar@smit.fit>
2024-02-12 09:56:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div class="bg-[#D4E3EC] flex-1 ">
+      <nav class="flex justify-around bg-[#24242B] text-[#EEEFF4] border-spacing-1 m-5 py-3">
+        <div>
+          <p className="text-1xl black-800">LOGOBAKERY</p>
+        </div>
+        <div>
+          <ul class="flex align-center">
+            <li class="px-5">Services</li>
+            <li class="px-5">Project</li>
+            <li class="px-5">About</li>
+          </ul>
+        </div>
+        <div>
+          <button class="bg-[#0089A9] text-[#8DE7FC] px-5 py-0.5 rounded-full">
+            Contact
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+}
